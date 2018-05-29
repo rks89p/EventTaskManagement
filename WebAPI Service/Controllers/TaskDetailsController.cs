c13640d812ed0a5abb19c544e9a254ce0b5577c9
Ram <rks89p@gmail.com>
2018-05-29 13:09:39 +0530

    Initial commit

---
+++ WebAPI Service/Controllers/TaskDetailsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI_Service;
+
+namespace WebAPI_Service.Controllers
+{
+    public class TaskDetailsController : ApiController
+    {
+        private DBTestEntities db = new DBTestEntities();
+
+        // GET: api/TaskDetails
+        public IQueryable<TaskDetail> GetTaskDetails()
+        {
+            return db.TaskDetails;
+        }
+
+        // GET: api/TaskDetails/5
+        [ResponseType(typeof(TaskDetail))]
+        public async Task<IHttpActionResult> GetTaskDetail(int id)
+        {
+            TaskDetail taskDetail = await db.TaskDetails.FindAsync(id);
+            if (taskDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(taskDetail);
+        }
+
+        // PUT: api/TaskDetails/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutTaskDetail(int id, TaskDetail taskDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != taskDetail.Task_ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(taskDetail).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/TaskDetails
+        [ResponseType(typeof(TaskDetail))]
+        public async Task<IHttpActionResult> PostTaskDetail(TaskDetail taskDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.TaskDetails.Add(taskDetail);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = taskDetail.Task_ID }, taskDetail);
+        }
+
+        // DELETE: api/TaskDetails/5
+        [ResponseType(typeof(TaskDetail))]
+        public async Task<IHttpActionResult> DeleteTaskDetail(int id)
+        {
+            TaskDetail taskDetail = await db.TaskDetails.FindAsync(id);
+            if (taskDetail == null)
+            {
+                return NotFound();
+            }
+
+            db.TaskDetails.Remove(taskDetail);
+            await db.SaveChangesAsync();
+
+            return Ok(taskDetail);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TaskDetailExists(int id)
+        {
+            return db.TaskDetails.Count(e => e.Task_ID == id) > 0;
+        }
+    }
+}
---
+++ WebAPI Service/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebAPI_Service")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebAPI_Service")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e210388-8b04-4a7f-abe2-002c21efa069")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
